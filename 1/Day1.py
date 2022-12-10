a538916b88e9c002db6b5a8e17082d3d39cac043
Thomas Van <thomasvan@Thomass-Mac-mini.local>
2022-12-09 18:09:35 -0700

    Completed DayOne problem

---
+++ 1/Day1.py
@@ -0,0 +1,72 @@
+# DAY 1 - Calorie Counting
+
+# Input - list of list of calories of each elf
+# Each list in list needs to be totaled and then compared
+
+from collections import namedtuple
+
+EXAMPLE_LIST = [[1000, 2000, 3000], [4000], [5000, 6000], [7000, 8000, 9000], [10000]]
+
+def main():
+
+    # Example list for testing
+    # print(EXAMPLE_LIST)
+    # calories_list = EXAMPLE_LIST
+    # print(calories_list)
+
+    # Get inputs into a list of lists
+    with open('calorie_list.txt') as f:
+        lines = f.read().splitlines()
+
+    # Remove spaces
+    lines = [line.replace(' ', '') for line in lines]
+
+    # Create list of list
+    calories_list = []
+    sub_list = []
+    for line in lines:
+        if line == '':
+            calories_list.append(sub_list)
+            sub_list = []
+        else:
+            sub_list.append(int(line))
+
+    elves = []
+    # Create list of elves with their id and total carrying calories
+    for calories in calories_list:
+        elves.append(sum(calories))
+
+    # Find the Elf with most calories
+    most_calories = 0
+    for elf in elves:
+        if elf >= most_calories:
+            most_calories = elf
+
+    # Alternatice
+    print(max(elves))
+
+    print("*****************************************************************************")
+    print(f"Elf with most calories: {most_calories} ")
+    print(f"Elf with most calories: {max(elves)} ")
+    print("*****************************************************************************")
+
+    num_of_elves = len(elves)
+    print(num_of_elves)
+
+    elves.sort()
+
+    sum_top_three = sum([elves[num_of_elves-1], elves[num_of_elves-2], elves[num_of_elves-3]])
+    print(sum_top_three)
+
+
+    # Sort the elves and find total of top three
+    # for elf in elves:
+    #     if elf.calories >= most_calories:
+    #         most_calories = elf.calories
+    #         elf_thats_packing = elf
+
+
+
+
+if __name__ == "__main__":
+    main()
--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # AdventOfCode2022
 --- Day 1: Calorie Counting ---
+
+.
+├── 1
+│   ├── Day1.py
+│   ├── calorie_list.txt
+│   └── problem.md
+├── 2
+├── 3
+├── 4
+├── 5
+├── 6
+├── 7
+├── 8
+├── 9
+└── README.md
